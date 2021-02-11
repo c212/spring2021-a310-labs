f4929d85b0ac577d6bb9bd7e9bd7c0e323f25a00
Suyash Poredi <suyashporedi01@gmail.com>
2021-02-11 16:08:30 -0500

    Delete function

--- lab04-feb-11/suyash-afternoon/AVL.py
+++ lab04-feb-11/suyash-afternoon/AVL.py
@@ -76,13 +76,74 @@ class AVL_Tree(object):
 
         return y
 
-tree = AVL_Tree()
-root = TreeNode(7)
-tree.insert(root, 3)
-tree.insert(root, 17)
-tree.insert(root,5)
-tree.insert(root, 13)
-print(root.right.val)
-new_root = tree.insert(root,15)
+    def delete(self, root, key):
+        if not root:
+            return root
 
-print(new_root.right.val)
+        elif key < root.val:
+            root.left = self.delete(root.left, key)
+
+        elif key > root.val:
+            root.right = self.delete(root.right, key)
+
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            temp = self.getMinValueNode(root.right)
+            root.val = temp.val
+            root.right = self.delete(root.right, temp.val)
+
+        root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+
+        balance = self.getBalance(root)
+
+        # Case 1  left left
+
+        if balance > 1 and self.getBalance(root.left) >= 0:
+            return self.rightRotate(root)
+
+    #     case 2 right right
+        if balance < -1 and self.getBalance(root.right) <= 0:
+            return self.leftRotate(root)
+    #     Case 3 Left Right
+        if balance > 1 and self.getBalance(root.left) < 0 :
+            root.left = self.leftRotate(root.left)
+            return self.rightRotate(root)
+        #     Case 4 Right Left
+        if balance < -1 and self.getBalance(root.right) > 0:
+            root.right = self.rightRotate(root.right)
+            return self.leftRotate(root)
+
+        return root
+    def getMinValueNode(self, root):
+        if root is None or root.left is None:
+            return root
+        return self.getMinValueNode(root.left)
+
+# tree = AVL_Tree()
+# root = TreeNode(7)
+# tree.insert(root, 3)
+# tree.insert(root, 17)
+# tree.insert(root,5)
+# tree.insert(root, 13)
+# print(root.right.val)
+# new_root = tree.insert(root,15)
+#
+# print(new_root.right.val)
+
+tree = AVL_Tree()
+
+root = TreeNode(17)
+tree.insert(root, 7)
+tree.insert(root, 21)
+tree.insert(root, 13)
+print(root.val)
+new_root = tree.delete(root, 17)
+print(new_root.val)
