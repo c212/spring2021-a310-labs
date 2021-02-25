bf3f32181c7f3977e6df17f5e51f2584f95ae363
joskrave <joskrave@iu.edu>
2021-02-25 15:30:53 -0500

    updated bloomfilter with h2

--- lab-06-afternnoon-feb-25/bloomFilter.py
+++ lab-06-afternnoon-feb-25/bloomFilter.py
@@ -6,3 +6,13 @@ def h(string, size):
         sum = sum + ord(letter)
     return sum % size
 print(h("Arsenal",len(filterArray)))
+
+def h2(string,size):
+    sum=0 
+    count = 0
+    for letter in string:
+        count = count +1
+        sum = sum + 2*count + ord(letter)
+    return sum % size
+
+print(h2("Arsenal",len(filterArray)))
