c61fc448de35d3ba6c76abda4e0613bedfb8f937
dustinbucher <dustin@dustinbucher.com>
2021-02-25 10:43:31 -0500

    Hash2 Done

--- lab06-feb-25/dustin-morning/bloomFilter.py
+++ lab06-feb-25/dustin-morning/bloomFilter.py
@@ -4,6 +4,18 @@ def hash(string, size):
     for letter in string:
         sum = sum + ord(letter)
     return sum%size
+def hash2(string, size):
+    sum = 0
+    count = 0
+    for letter in string:
+        count = count + 1
+        sum = sum + 3*count + ord(letter)
+    return sum % size
+
+
 
 print(hash("morning", len(filter)))
+print(hash("sunshine", len(filter)))
+print(hash2("morning", len(filter)))
+print(hash2("sunshine", len(filter)))
 
