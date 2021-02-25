d3d1020d559e16cfe2993dd4683bb31955ac7fd4
heatahm <61248203+heatahm@users.noreply.github.com>
2021-02-25 10:40:16 -0500

    Added hash2

--- lab-06-feb-25/lab6.py
+++ lab-06-feb-25/lab6.py
@@ -1,4 +1,4 @@
-filter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+filter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # 0 - 12, 13 total
 
 def hash(string, size):
     sum = 0
@@ -8,4 +8,19 @@ def hash(string, size):
 
     return sum % size
 
-print(hash("morning", len(filter)))
+def hash2(string, size):
+    sum = 0
+    count = 0
+    for letter in string:
+        count = count + 1
+        sum = sum + 3*count + ord(letter)
+    return sum % size
+
+print(hash("morning", len(filter))) #in the sample, 8 and 10
+print(hash("sunshine", len(filter))) #in the sample, 5 and 6
+#If a word is 5 and 10, the Bloom filter says it might be in the set
+#In the sample, "tight" fnv = 10, and murmur = 8, so it is a maybe
+
+print(hash2("morning", len(filter))) 
+print(hash2("sunshine", len(filter)))
+
