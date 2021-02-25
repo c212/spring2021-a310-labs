638e11443aa9bd870a936af455c1e4f9f59161f8
dustinbucher <dustin@dustinbucher.com>
2021-02-25 11:08:25 -0500

    Conversion

--- lab06-feb-25/dustin-morning/bloomFilter.py
+++ lab06-feb-25/dustin-morning/bloomFilter.py
@@ -11,11 +11,24 @@ def hash2(string, size):
         count = count + 1
         sum = sum + 3*count + ord(letter)
     return sum % size
+def hash3(string, size):
+    sum = 0
+    for letter in string:
+        sum = sum +ord(letter)
+    return  ((sum+2) *5) %size
 
+def conversion(string, size):
+    return(hash(string, size),hash2(string, size),hash3(string, size))
 
 
 print(hash("morning", len(filter)))
 print(hash("sunshine", len(filter)))
+print(hash("lecture",len(filter)))
 print(hash2("morning", len(filter)))
 print(hash2("sunshine", len(filter)))
+print(hash2("lecture",len(filter)))
+print(hash3("morning", len(filter)))
+print(hash3("sunshine", len(filter)))
+print(hash3("lecture",len(filter)))
+print(conversion("morning",len(filter)))
 
