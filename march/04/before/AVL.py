5474ef1c4ca6182c0cdae77d5bd27b55f60b6bb8
c212 <40395648+c212@users.noreply.github.com>
2021-03-03 22:40:53 -0500

    adding the adjust function to the AVL class

--- march/04/before/AVL.py
+++ march/04/before/AVL.py
@@ -1,21 +1,21 @@
 from BST import *
 
 class AVL(BST):
-  def depth(self):
-    if self.left == None and self.right == None:
-      return 1
-    elif self.left == None:
-      return 1 + self.right.depth()
-    elif self.right == None:
-      return 1 + self.left.depth()
-    else:
-      return 1 + max(self.left.depth(), self.right.depth())
-  def balance(self):
-    if self.left == None and self.right == None:
-      return 0
-    elif self.left == None:
-      return 0 - self.right.depth()
-    elif self.right == None:
-      return self.left.depth()
-    else:
-      return self.left.depth() - self.right.depth()
+  def depth(self): #--------------------------------------(depth)-----
+    if self.left == None and self.right == None: return 1
+    elif self.left == None: return 1 + self.right.depth()
+    elif self.right == None: return 1 + self.left.depth()
+    else: return 1 + max(self.left.depth(), self.right.depth())
+  def balance(self): #------------------------------------(balance)---
+    if self.left == None and self.right == None: return 0
+    elif self.left == None: return 0 - self.right.depth()
+    elif self.right == None: return self.left.depth()
+    else: return self.left.depth() - self.right.depth()
+  def adjust(self): #-------------------------------------(adjust)-----
+    if self.left == None: left = None
+    else: left = self.left.adjust()
+    if self.right == None: right = None
+    else: right = self.right.adjust()
+    return AVL(self.key + 1, left, right)
+  #-------------(all three functions are purely functional)-------------
+
--- march/04/before/tests-AVL.py
+++ march/04/before/tests-AVL.py
@@ -8,11 +8,25 @@ a.display()
 print("root depth is: ", a.depth())
 print("root balance is: ", a.balance())
 
-#  3_
-# /  \
-# 1  5
-#   /
-#   4
-# root depth is:  3
-# root balance is:  -1
+a = a.adjust() # this is the new addition 
+
+a.display()
+
+print("root depth is: ", a.depth())
+print("root balance is: ", a.balance())
+
+#   3_
+#  /  \
+#  1  5
+#    /
+#    4
+#  root depth is:  3
+#  root balance is:  -1
+#   4_
+#  /  \
+#  2  6
+#    /
+#    5
+#  root depth is:  3
+#  root balance is:  -1
 
