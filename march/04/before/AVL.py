eed630c7433456e55bb8ceabf179055de1fb7549
c212 <40395648+c212@users.noreply.github.com>
2021-03-03 22:36:12 -0500

    adding balance to AVL nodes

--- march/04/before/AVL.py
+++ march/04/before/AVL.py
@@ -10,3 +10,12 @@ class AVL(BST):
       return 1 + self.left.depth()
     else:
       return 1 + max(self.left.depth(), self.right.depth())
+  def balance(self):
+    if self.left == None and self.right == None:
+      return 0
+    elif self.left == None:
+      return 0 - self.right.depth()
+    elif self.right == None:
+      return self.left.depth()
+    else:
+      return self.left.depth() - self.right.depth()
--- march/04/before/tests-AVL.py
+++ march/04/before/tests-AVL.py
@@ -5,12 +5,14 @@ a.insert(AVL(1))
 a.insert(AVL(5))
 a.insert(AVL(4))
 a.display()
-print("depth is: ", a.depth())
+print("root depth is: ", a.depth())
+print("root balance is: ", a.balance())
 
 #  3_
 # /  \
 # 1  5
 #   /
 #   4
-# depth is:  3
-# 
+# root depth is:  3
+# root balance is:  -1
+
